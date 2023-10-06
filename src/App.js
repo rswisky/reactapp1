e1493b1efae8260254b27e8864bebf8b39da3666
ryan sulier <rswisky@gmail.com>
2023-10-06 16:54:16 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+ 
+function App() {
+    return (
+        <h1> Hello this is my 2nd Web APP in AWS. </h1>
+        
+    );
+}
+ 
+export default App;
+
+
