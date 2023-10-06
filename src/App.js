ceffaf590a1aeaa51458692cc85231c0bb4e1b91
rswisky <rysulier@gmail.com>
2023-10-06 17:26:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
     return (
         <h1> Hello My name is  Ryan Sulier this is my 2nd Web APP in AWS. </h1>
-        <p> I am using react.js and Github to update this on a regular basis. Please comeback for updates soon!</p>
     );
 }
  
