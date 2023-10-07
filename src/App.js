67e67dfb74cd5b59e3444218217d5943e59526c1
rswisky <rysulier@gmail.com>
2023-10-07 13:25:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import myImage from './glacier.jpg';
+import logo from './logo.svg'
 function App() {
     return (
         <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>        
           <h1 style={{'text-align':'center'}}>Hello My name is Ryan Sulier this is my 2nd Web app in AWS.</h1> 
           <p style={{'text-align':'center'}}>If you like this send me a text at 650-743-1490</p>
           <img src={myImage} alt="My Image" />
