bb008a834e59b53fa72a78f7f5e90cf0fab3cabf
rswisky <rysulier@gmail.com>
2023-10-07 13:26:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
           <h2>Welcome to React</h2>        
           <h1 style={{'text-align':'center'}}>Hello My name is Ryan Sulier this is my 2nd Web app in AWS.</h1> 
           <p style={{'text-align':'center'}}>If you like this send me a text at 650-743-1490</p>
-          <img src={myImage} alt="My Image" />
            <storage/>
        </div>
     );
