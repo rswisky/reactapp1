779fb85bdfb364888dead455c72328d2ccbea53f
rswisky <rysulier@gmail.com>
2023-10-06 23:26:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-import Image from 'c:/glacier.jpg'
 function App() {
     return (
         <div>
-          <img src={Image} alt="Image"/>
           <h1 style={{'text-align':'center'}}>Hello My name is Ryan Sulier this is my 2nd Web app in AWS.</h1> 
           <p style={{'text-align':'center'}}>If you like this send me a text at 650-743-1490</p>
            <storage/>
