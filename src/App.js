e2c6f4198ff0a5f9bb6120aeefc1427b63ba19bb
rswisky <rysulier@gmail.com>
2023-10-06 21:09:36 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
     return (
         <div>
-          <h1 style={{'text-align':'center'}}>Hello My name is Ryan Sulier this is my 2nd Web app in AWS.
-            If you like it send me a text 650-743-1490/h1>    
+          <h1 style={{'text-align':'center'}}>Hello My name is Ryan Sulier this is my 2nd Web app in AWS.If you like it send me a text 650-743-1490</h1>    
           
            <storage/>
        </div>
