97e159db12804e27e671d8cf81b31bcf49805169
rswisky <rysulier@gmail.com>
2023-10-09 17:05:31 -0400

    z

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import logo from './logo.svg'
 function App() {
     return (
         <div>
-            <div className="bg"></div>
+           
           <img src={logo} className="App-logo" alt="logo" />
            <img src="https://media.licdn.com/dms/image/D5603AQETAUKZ8ME-aA/profile-displayphoto-shrink_200_200/0/1669208783702?e=1701907200&v=beta&t=gH2FYdLRQaBWzg5T6w5pmrnvsgatmc35wSRF1tazA-Q"/>       
    
           <h2>Welcome to React</h2>        
           <h1 style={{'text-align':'center'}}>Hello My name is Ryan Sulier this is my 2nd Web app in AWS.</h1> 
           <p style={{'text-align':'center'}}>If you like this send me a text at 650-743-1490</p>
+          <div className="bg"></div>
            <storage/>
        </div>
     );
